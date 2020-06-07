d87079eb33b4b87d5633511ac6592bad3a039dc3
Leevi Kukkonen <K8397@student.jamk.fi>
2020-06-07 23:40:50 +0300

    okay this is new site let us see if this works

---
+++ precache-manifest.d9558a9b9548ad6f3c3688fae7f96617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129aaaf523ff90960cb5b92401abb117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6575ba9a93b5d91383",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a5e8b4b14072248881c",
+    "url": "/static/js/2.fedb4ad1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fedb4ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6575ba9a93b5d91383",
+    "url": "/static/js/main.3c2ba6f5.chunk.js"
+  },
+  {
+    "revision": "02d5081a414a737d19c0",
+    "url": "/static/js/runtime-main.70759dc3.js"
+  }
+]);
